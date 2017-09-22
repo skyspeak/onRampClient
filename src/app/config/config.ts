f9a4d1a41013e31bdba13b8cd0b028ce9a874e53
Megh Gautam <skyspeak@gmail.com>
2017-09-22 10:46:40 -0700

    updated links
    updated links

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CustomConfigs{
 
-    private apiURL:String= "https://whispering-forest-40948.herokuapp.com/api/";
-    private baseURL:String="https://pacific-headland-22092.herokuapp.com/";
+    private apiURL:String= "https://onrampadmin.herokuapp.com/api/";
+    private baseURL:String="https://onrampclient.herokuapp.com/";
 
     getAPIURL(){
         return this.apiURL;
